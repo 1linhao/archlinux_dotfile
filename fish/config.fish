dc40bf920c8680a22c772e1492f1bd1b914ebb72
1linhao <2931800349@qq.com>
2025-08-23 23:55:01 +0800

    style(fish, yazi): 优化以下配置文件的内容结构和注释 fish/config.fish, yazi/keymap.toml
    chore(fish):
    添加abbr: `sudo journalctl -fu`
    同步`yazi/keymap.toml`的目录跳转

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,50 @@ end
 
 set -gx PATH $PATH ~/.local/share/nvim/mason/bin/
 
+# keybind
+abbr m musicfox
+abbr r rmpc
+abbr n ncmpcpp
+abbr f fanyi
+abbr b btop
+abbr e exit
+
+# 目录跳转
+## 文档目录
+abbr gdd cd /home/lh/Downloads
+abbr gdpp cd /home/lh/Pictures
+abbr gdpg cd /home/lh/Pictures/Gallery
+abbr gdpw cd /home/lh/Pictures/Wallpapers
+abbr gdps cd /home/lh/Pictures/Screenshots
+abbr gdvv cd /home/lh/Videos
+abbr gdvr cd /home/lh/Videos/Recordings
+abbr gdm cd /home/lh/Music
+abbr gdo cd /home/lh/Public/share/obsidian
+abbr gdgg cd /home/lh/git
+abbr gdgd cd /home/lh/git/dotfile
+abbr gdgc cd /home/lh/git/caelestia
+## config目录
+abbr gcc cd /home/lh/.config
+abbr gcy cd /home/lh/.config/yazi
+abbr gcg cd /home/lh/.config/go-musicfox
+abbr gck cd /home/lh/.config/kitty
+abbr gch cd /home/lh/.config/hypr
+abbr gcn cd /home/lh/.config/nvim
+abbr gcf cd /home/lh/.config/fish
+## cache目录
+abbr gCC cd /home/lh/.cache
+abbr gCg cd /home/lh/.cache/go-musicfox
+## local目录
+abbr gls cd /home/lh/.local/share
+abbr glt cd /home/lh/.local/state
+## share目录
+abbr gss cd /usr/share
+## 程序目录
+### WallpaperEngineData for steam
+abbr gasw cd /home/lh/.steam/steam/steamapps/workshop/content/431960
+### 微信数据目录
+abbr gawd cd /home/lh/.local/share/WeChat_Data
+
 # yazi 
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
@@ -14,45 +58,6 @@ function y
     rm -f -- "$tmp"
 end
 
-abbr m musicfox
-abbr r rmpc
-abbr n ncmpcpp
-abbr f fanyi
-abbr b btop
-abbr e exit
-
-# dotfile
-function df -d "Manage dotfiles with git"
-  set -l DOTFILES_DIR "$HOME/dotfiles/"
-  set -l WORK_TREE "$HOME"
-  set -l DOTFILES \
-    $HOME/.bashrc \
-    $HOME/.config/fcitx5/* \
-    $HOME/.config/fontconfig/* \
-    $HOME/.config/fish/config.fish \
-    # $HOME/.config/hypr/* \
-    $HOME/.config/kitty/* \
-    # $HOME/.config/mako/* \
-    $HOME/.config/mpd/mpd.conf \
-    $HOME/.config/ncmpcpp/bindings \
-    $HOME/.config/ncmpcpp/config \
-    $HOME/.config/nvim/* \
-    $HOME/.config/rmpc/* \
-    # $HOME/.config/rofi/* \
-    # $HOME/.config/waybar/* \
-    $HOME/.config/yazi/* \
-    $HOME/Script/*
-
-  switch $argv[1]
-    case add
-      git --git-dir=$DOTFILES_DIR --work-tree=$WORK_TREE add -f $DOTFILES
-    case restore
-      git --git-dir=$DOTFILES_DIR --work-tree=$WORK_TREE restore --staged $DOTFILES
-    case '*'
-      git --git-dir=$DOTFILES_DIR --work-tree=$WORK_TREE $argv
-  end
-end
-
 # neovim
 abbr snvim sudo -E nvim
 
@@ -61,6 +66,7 @@ abbr spo systemctl poweroff
 abbr sre systemctl reboot
 abbr ssu systemctl suspend
 abbr shi systemctl hibernate
+abbr sjfu sudo journalctl -fu
 abbr sstart sudo systemctl start
 abbr sstop sudo systemctl stop
 abbr srestart sudo systemctl restart
